0e7b75434918df120a2538d83d5c3b81c920902d
Michelle Ross <miross629@gmail.com>
2023-05-17 11:58:37 -0700

    Update form data

--- qwixx-score-card/form-data.js
+++ qwixx-score-card/form-data.js
@@ -1,38 +1,46 @@
 export default [
   // row 1
   {
-    id: "rowOne",
+    id: 1,
+    name: "rowOne",
+    label: "Row One",
     color: "red",
     inputs: 11,
-    startSpace: 2,
-    spaces: [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+    startBoxNum: 2,
+    boxNums: [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
     lockRow: false,
   },
   // row 2
   {
-    id: "rowTwo",
+    id: 2,
+    name: "rowTwo",
+    label: "Row Two",
     color: "yellow",
     inputs: 11,
-    startSpace: 2,
-    spaces: [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+    startBoxNum: 2,
+    boxNums: [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
     lockRow: false,
   },
   // row 3
   {
-    id: "rowThree",
+    id: 3,
+    name: "rowThree",
+    label: "Row Three",
     color: "green",
     inputs: 11,
-    startSpace: 12,
-    spaces: [12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2],
+    startBoxNum: 12,
+    boxNums: [12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2],
     lockRow: false,
   },
   // row 4
   {
-    id: "rowFour",
+    id: 4,
+    name: "rowFour",
+    label: "Row Four",
     color: "blue",
     inputs: 11,
-    startSpace: 12,
-    spaces: [12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2],
+    startBox: 12,
+    boxNums: [12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2],
     lockRow: false,
   },
 ];
