d2d9614c943fc49171ae50e52807649c8af0317d
Michelle Ross <miross629@gmail.com>
2023-05-16 22:24:22 -0700

    Create form data as an array of objects

---
+++ qwixx-score-card/form-data.js
@@ -0,0 +1,38 @@
+export default [
+  // row 1
+  {
+    id: "rowOne",
+    color: "red",
+    inputs: 11,
+    startSpace: 2,
+    spaces: [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+    lockRow: false,
+  },
+  // row 2
+  {
+    id: "rowTwo",
+    color: "yellow",
+    inputs: 11,
+    startSpace: 2,
+    spaces: [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+    lockRow: false,
+  },
+  // row 3
+  {
+    id: "rowThree",
+    color: "green",
+    inputs: 11,
+    startSpace: 12,
+    spaces: [12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2],
+    lockRow: false,
+  },
+  // row 4
+  {
+    id: "rowFour",
+    color: "blue",
+    inputs: 11,
+    startSpace: 12,
+    spaces: [12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2],
+    lockRow: false,
+  },
+];
