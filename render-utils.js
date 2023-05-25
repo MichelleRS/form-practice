7e8270404eb3e826ce0db716c992fffbd0a86c99
Michelle Ross <miross629@gmail.com>
2023-05-25 13:14:58 -0700

    Set value attribute for each checkbox

--- render-utils.js
+++ render-utils.js
@@ -98,6 +98,8 @@ export function renderCheckboxInput(rowData, boxNum) {
   // set name and id attributes, ex: rowOneBox1
   inputEl.setAttribute("name", `${rowData.name}Box${boxNum}`);
   inputEl.setAttribute("id", `${rowData.name}Box${boxNum}`);
+  // set value
+  inputEl.setAttribute("value", boxNum);
   // set class attribute
   inputEl.classList.add(`${rowData.name}Box`);
 
